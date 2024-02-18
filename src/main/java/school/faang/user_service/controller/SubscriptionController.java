131d190483c0c08758436aacc52581c9b6759453
Tupik Maxim <m-tupik2006@mail.ru>
2024-02-19 04:00:54 +0700

    unsubscribe to a user

---
+++ src/main/java/school/faang/user_service/controller/SubscriptionController.java
@@ -0,0 +1,20 @@
+package school.faang.user_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import school.faang.user_service.service.SubscriptionService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/subscription")
+public class SubscriptionController {
+    private final SubscriptionService subscriptionService;
+
+    @PostMapping("/unfollow/{followerId}/{followeeId}")
+    public void unfollowUser(@PathVariable long followerId, @PathVariable long followeeId) {
+        subscriptionService.unfollowUser(followerId, followeeId);
+    }
+}
